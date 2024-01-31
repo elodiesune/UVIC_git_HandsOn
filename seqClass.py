7fc3badc63bc8f46087b8b61ca7c0457b3fb2a24
elodiesune <elodiesune@gmail.com>
2024-01-31 17:55:23 +0100

    add comments

--- seqClass.py
+++ seqClass.py
@@ -1,7 +1,10 @@
+#import necessary libraries
 import sys, re
 from argparse import ArgumentParser
 
+#Define the goal of the function
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
+# Define parsers
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
@@ -10,8 +13,9 @@ if len(sys.argv) == 1:
     sys.exit(1)
 
 args = parser.parse_args()
-
+# Make sequence in upper case
 args.seq = args.seq.upper()
+# Research special letters to identify if it is DNA or RNA
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -22,6 +26,7 @@ if re.search('^[ACGTU]+$', args.seq):
 else:
     print ('The sequence is not DNA nor RNA')
 
+# Research a motif
 if args.motif:
     args.motif = args.motif.upper()
     print(f'BLABLA Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
