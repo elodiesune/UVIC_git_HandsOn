027bee8482ad694d9aee0511ea6b4161079b2a43
elodiesune <elodiesune@gmail.com>
2024-02-01 09:20:12 +0100

    simplify printing loop

--- nucleoperc.py
+++ nucleoperc.py
@@ -11,6 +11,7 @@ if len(sys.argv) == 1:
 
 # Set args as the output of the parser script
 args = parser.parse_args()
+
 # Convert to uppercase
 args.seq = args.seq.upper()
 
@@ -25,38 +26,21 @@ if 'T' in args.seq and 'U' in args.seq:
     sys.exit(1)
 
 # Set up count variables
-Ucount = 0
-Tcount = 0
-Acount = 0
-Gcount = 0
-Ccount = 0
+counts = {}
 
 sequence_length = len(args.seq)
 
 # Set up loop to check the letters and increase count per letter
 for letter in args.seq:
-    if letter == 'U':
-        Ucount += 1
-    elif letter == 'T':
-        Tcount += 1
-    elif letter == 'A':
-        Acount += 1
-    elif letter == 'G':
-        Gcount += 1
-    elif letter == 'C':
-        Ccount += 1
+	if letter in counts:
+		counts[letter] += 1
+	else:
+		counts[letter] = 1
 
-if Ucount > 0 and Tcount == 0:
-    print('The sequence is RNA')
-    print(f'%U: {(Ucount / sequence_length) * 100:.2f}%')
-    print(f'%G: {(Gcount / sequence_length) * 100:.2f}%')
-    print(f'%C: {(Ccount / sequence_length) * 100:.2f}%')
-    print(f'%A: {(Acount / sequence_length) * 100:.2f}%')
+if "U" in args.seq:
+	print('The sequence is RNA')
+else:
+	print('The sequence is DNA')
 
-elif Ucount == 0 and Tcount > 0:
-    print('The sequence is DNA')
-    print(f'%T: {(Tcount / sequence_length) * 100:.2f}%')
-    print(f'%G: {(Gcount / sequence_length) * 100:.2f}%')
-    print(f'%C: {(Ccount / sequence_length) * 100:.2f}%')
-    print(f'%A: {(Acount / sequence_length) * 100:.2f}%')
-
+for letter, count in counts.items():
+	print(f"%{letter}: {count / sequence_length * 100:.2f}%")
